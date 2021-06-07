3181d70aa4bdd22a009db01ec0b1c5a4a073930e
lucas-abratti <lucasabratti99@gmail.com>
2021-06-07 17:12:25 -0300

    preparing for merge

--- dashboard/src/components/SellsWidget.jsx
+++ dashboard/src/components/SellsWidget.jsx
@@ -21,6 +21,10 @@ function SellsWidget() {
 
     //let productsId = sells.split('-')
 
+    let previous;
+    let repeat = 0;
+    let next;
+
     return (
         <div>
             <h3 className="component-title">Cantidad total de ventas: {sells.length}</h3>
@@ -28,39 +32,41 @@ function SellsWidget() {
                 {sells.length === 0 && <p>Cargando...</p>}
                 {
                     sells.map((sell, i) => {
-                        let previous = sells[i];
-                        let repeat = 1;
-                        let repeat2 = 0;
-                        sell.forEach(pId => {
-                            if (pId === previous) {
-                                repeat2 +=1
-                                console.log(`La id ${pId} se repite ${repeat2} veces`)
-                            } else if (pId != previous){                        
-                                repeat2 = 1              
-                                previous = pId   
-                            }
-                            return repeat2                    
-                        });
+                        console.log(sells[i])
+                        repeat = 1;
+                        console.log('----------------------------------------')
                         return (
-                            <li key={i} className="sells-li">
-                                <h3>{`${sell}`}</h3>
-                                {
-                                    sell.map((prodId, i) => {
-                                        if (prodId === previous) {
-                                            repeat++
-                                        } else {
-                                            previous = prodId
-                                            return (
-                                                <div key={i} className="sellsProdImg">
-                                                    <h3>{`x${repeat}`}</h3>
-                                                    <img src={`http://localhost:3001/api/product-image/${prodId}`} width="100px" alt="product" />
-                                                </div>
-                                            )
-                                        }
-                                    })
-                                }
-
-                            </li>
+                            <>
+                                <h2>{sell}</h2>
+                                <ul className="sells-ul">
+                                    {
+                                        sell.map((prodId, i) => {
+                                            previous = sell[i - 1];
+                                            next = sell[i + 1]
+                                            if (prodId !== previous && prodId !== next) {
+                                                repeat = 1
+                                                return (
+                                                    <div key={i} className="sellsProdImg">
+                                                        <h3>{`${prodId} x ${repeat}`}</h3>
+                                                        <img src={`http://localhost:3001/api/product-image/${prodId}`} width="100px" alt="product" />
+                                                    </div>
+                                                )
+                                            } else {
+                                                repeat += 1
+                                                if (prodId !== next){
+                                                    return (
+                                                        <div key={i} className="sellsProdImg">
+                                                            <h3>{`${prodId} x ${repeat}`}</h3>
+                                                            <img src={`http://localhost:3001/api/product-image/${prodId}`} width="100px" alt="product" />
+                                                        </div>
+                                                    )
+                                                }
+                                                return
+                                            }
+                                        })
+                                    }
+                                </ul>
+                            </>
                         )
                     })
                 }
